cc3bdc060c2044aa4199fd3219c846df08a65455
habitatum <direcciondeproyectos@infohabitatum.com>
2026-07-25 18:18:30 -0500

    Add files via upload

--- habitatum-oc-web/app/login/page.js
+++ habitatum-oc-web/app/login/page.js
@@ -24,7 +24,7 @@ export default function Login() {
       setError('Correo o contraseña incorrectos.');
       return;
     }
-    router.push('/dashboard');
+    router.push('/proyectos');
   }
 
   return (
