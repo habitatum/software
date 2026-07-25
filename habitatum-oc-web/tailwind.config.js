ae360959ad2a7642101e0dc3c064e5134008606d
habitatum <direcciondeproyectos@infohabitatum.com>
2026-07-25 17:41:53 -0500

    Add files via upload

--- habitatum-oc-web/tailwind.config.js
+++ habitatum-oc-web/tailwind.config.js
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        // Paleta de marca HABITATUM
+        dorado: '#b88a52',
+        'gris-calido': '#cdc5ba',
+        hueso: '#efece6',
+        carbon: '#2e2e2e',
+      },
+      fontFamily: {
+        marca: ['var(--font-cormorant)', 'serif'],
+      },
+    },
+  },
   plugins: [],
 };
